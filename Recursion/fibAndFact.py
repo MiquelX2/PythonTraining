c4d82849eed9b78f81a22a57612334cb193da10b
Miquel <micdes1@gmail.com>
2018-07-22 21:44:10 +0200

    Factorial implementation via recursion

--- Recursion/fibAndFact.py
+++ Recursion/fibAndFact.py
@@ -1,4 +1,5 @@
-def fibb(n):
+import math
+def fibbRecursive(n):
 	result = None
 	if n < 0:
 		print("You are trying to cheat me! Game over, you're done!")
@@ -9,10 +10,20 @@ def fibb(n):
 	else:
 		result = fibb(n-1)+ fibb(n-2)
 	return result
+def factorialRecursive(n):
+	result = None
+	if n<0:
+		print("You're trying to cheat me! That's enough, game over!")
+	elif n < 2:
+		result = 1
+	else:
+		result = n*factorialRecursive(n-1)
+	return result
 
 def Main():
 	print("I'm going to print fibbonacci and factorial calculation results")
-	print(fibb(-6))
+	print(factorialRecursive(1000))
+
 	
 if __name__ == "__main__":
 	Main()
