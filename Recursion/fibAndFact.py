e6e161a5eb3d0ca65390314d10ba6a19bb3d3ca0
Miquel <micdes1@gmail.com>
2018-07-22 21:37:57 +0200

    Fibbonacci implementation via recursion

--- Recursion/fibAndFact.py
+++ Recursion/fibAndFact.py
@@ -1,5 +1,18 @@
+def fibb(n):
+	result = None
+	if n < 0:
+		print("You are trying to cheat me! Game over, you're done!")
+	elif n == 0:
+		result = 0
+	elif n == 1:
+		result = 1
+	else:
+		result = fibb(n-1)+ fibb(n-2)
+	return result
+
 def Main():
 	print("I'm going to print fibbonacci and factorial calculation results")
+	print(fibb(-6))
 	
 if __name__ == "__main__":
 	Main()
