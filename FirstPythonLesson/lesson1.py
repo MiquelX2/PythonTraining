2a783376d9236f54143a32364cd0a5dfb1952b96
Miquel <micdes1@gmail.com>
2018-07-22 12:12:46 +0200

    Test of -a parameter to avoid git add command

--- FirstPythonLesson/lesson1.py
+++ FirstPythonLesson/lesson1.py
@@ -1,6 +1,7 @@
 def Main():
 	print("Hello World")
-	print("I did second change")
+	# print("I did second change")
+	print("To add the change automatically to stash you need to use -a parameter of git commit command")
 
 	
 if __name__ == "__main__":
